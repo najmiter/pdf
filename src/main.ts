ab5d76a97fa6f1e53ab7187c917439092d99396d
Najam <realnajmiter@gmail.com>
2025-08-31 15:57:33 +0500

    chore: link worker from cdn

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { GlobalWorkerOptions } from 'pdfjs-dist';
 
-createApp(App).mount('#app')
+GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.js';
+
+createApp(App).mount('#app');
